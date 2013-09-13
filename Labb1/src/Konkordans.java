9d5aa138bada1376ba4169531b37c8d84e605c56
elincl <elincl@kth.se>
2013-09-13 15:59:36 +0200

    hehj

--- Labb1/src/Konkordans.java
+++ Labb1/src/Konkordans.java
@@ -8,20 +8,17 @@ import java.util.Scanner;
 public class Konkordans {
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		
-		HEJ ELIN!
-		
-		
+
 	}
-	
 	
 	public static boolean createIndex() throws FileNotFoundException, UnsupportedEncodingException{
 
-		Scanner in = new Scanner(new File("/var/tmp/elincl_ut"), "ISO-8859-1");
+	}
+		public static booelan createHash(){
+			
 		
+		Scanner in = new Scanner(new File("/var/tmp/elincl_ut"), "ISO-8859-1");
 		PrintWriter writer = new PrintWriter("A.txt", "UTF-8");
-
-
 		String lastWord = "";
 		String line = "";
 		while(in.hasNextLine()){
@@ -32,18 +29,14 @@ public class Konkordans {
 				newWord = line.substring(0, shortWord[0].length());
 			else
 				newWord = line.substring(0, 3);
-
 			if(!newWord.equals(lastWord)){
 				String[] lineNr = line.split(" ");
 				String a = newWord + " " + lineNr[1];
 				writer.println(a);
 				lastWord = newWord;
 			}
-
 		}
-
 		writer.close();
-		
 		return true;
 		}
 
