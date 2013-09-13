e8aca5f26abbcbb332bc6dad94a3ddf35f7aca32
elincl <elincl@kth.se>
2013-09-13 16:12:57 +0200

    kodskelett klart

--- Labb1/src/Konkordans.java
+++ Labb1/src/Konkordans.java
@@ -8,15 +8,18 @@ import java.util.Scanner;
 public class Konkordans {
 
 	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-
+		createIndexes();
+		
+		//SEARCH
 	}
 	
-	public static boolean createIndex() throws FileNotFoundException, UnsupportedEncodingException{
-
+	public static boolean createIndexes() throws FileNotFoundException, UnsupportedEncodingException{
+//		createBucket();
+		createWordIndex();
+		return true;
 	}
-		public static booelan createHash(){
-			
-		
+		public static boolean createBucket() throws FileNotFoundException, UnsupportedEncodingException{
+
 		Scanner in = new Scanner(new File("/var/tmp/elincl_ut"), "ISO-8859-1");
 		PrintWriter writer = new PrintWriter("A.txt", "UTF-8");
 		String lastWord = "";
@@ -39,7 +42,30 @@ public class Konkordans {
 		writer.close();
 		return true;
 		}
+		
+		public static boolean createWordIndex() throws FileNotFoundException, UnsupportedEncodingException{
+			
+			Scanner in = new Scanner(new File("/var/tmp/elincl_ut"), "ISO-8859-1");
+			PrintWriter writer = new PrintWriter("WordIndex.txt", "UTF-8");
+			String lastWord = "";
+			String line = "";
+			int pos = 0;
+			while(in.hasNextLine()){
+				line = in.nextLine();
+				pos += line.length()+1;
+				String[] shortWord = line.split(" ");
+				String newWord = "";
+				if(!newWord.equals(lastWord)){
+					String[] lineNr = line.split(" ");
+					String a = newWord + " " + pos + "\n";
+					writer.println(a);
+					lastWord = newWord;
+				}
+			}
+			writer.close();
+			
+			return true;
+		}
 
 
 }
-
