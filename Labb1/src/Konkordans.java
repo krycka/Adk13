9c2ed6b9dbd1c5be38bdd22451700575988b3c94
krycka <rodoo@csc.kth.se>
2013-09-16 17:08:25 +0200

    Main parts done, some buggfixes left

--- Labb1/src/Konkordans.java
+++ Labb1/src/Konkordans.java
@@ -1,11 +1,10 @@
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
@@ -22,6 +21,8 @@ import java.util.Scanner;
  * - kontrollera case insensitive
  * - lägg in om det är många resultat för en sökning (Visa alla?)
  * - spara res mot korpus i länkad lista
+ * - Två longs per bokstavskomb i bucket? dvs från offset, till offset.
+ * 
  */
 
 
@@ -30,33 +31,46 @@ public class Konkordans {
 	private static File tokfile;
 	private static File bucket;
 	private static File wordIndex;
-	private static final boolean DBG = true;
 	private static Time searchTime;
+	private static int resultsLimit = 25;
+	private static final boolean DBG = true;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		if(args.length == 0) System.out.println("Du måste skriva ett ord för att söka.");
-		if(args.length > 1) System.out.println("Du kan bara söka efter ett ord åt gången");
+		// Kontrollera att vi har fått ett ord att söka efter
+		if(args.length == 0) {	System.out.println("Du måste skriva ett ord för att söka."); 	System.exit(-1); }
+		if(args.length > 1) {	System.out.println("Du kan bara söka efter ett ord åt gången");	System.exit(-1); }
 
+		// Initiera filerna vi behöver
+		korpus = new File("/info/adk13/labb1/korpus");
+		tokfile = new File("/var/tmp/tokfile");
+		bucket = new File("/var/tmp/bucket");
+		wordIndex = new File("/var/tmp/wordIndex");
+		
+		// Kontrollera att indexfilerna, skapa dom om någon saknas.
 		if(!checkIndexes()) {
 			System.out.println("En eller fler filer saknas, genererar nya filer...");
 			createIndexes();
-		}
-		
-		// Indexer finns garanterat nu! Fortsätt med sökningen.
+		}		
+		// Nu är vi garanterade att ha de filer vi behöver
 		searchTime = new Time();
 		searchTime.start();
 		
-		// Hämta position i wordIndex från bucket
+		// Hämta position att söka på i wordIndex från bucket
 		RandomAccessFile raf = new RandomAccessFile(bucket, "r");
-		raf.seek(getBucketPosition(args[0]));
+		raf.seek(getBucketOffset(args[0]));
 		long wordIndexPos = raf.readLong();
-		long wordIndexNextPos = wordIndexPos+8;
+		long wordIndexNextPos = raf.readLong();
+		for(int i=0; i<100 && wordIndexNextPos == 0; i++) 
+			wordIndexNextPos = raf.readLong();
+		raf.close();
+		
+		// Hämta offset för ordet i tokfile från wordIndex
+		raf = new RandomAccessFile(wordIndex, "r");
+		raf.seek(wordIndexPos);
 		long tokfilePos = 0;
 		long tokfileNextPos = 0;
-		
-		// Hämta offsett för ordet i tokfile
-		raf = new RandomAccessFile(wordIndex, "r");
 		boolean found = false;
+		
 		for(long i=wordIndexPos; i<wordIndexNextPos && !found; i=raf.getFilePointer()) {
 			String line[] = raf.readLine().split(" ");
 			if(line[0].equals(args[0])) {
@@ -68,33 +82,45 @@ public class Konkordans {
 				found = true;
 			}
 		}
+		raf.close();
 		
-		System.out.println(searchTime.stop());
-		// Hämta alla offsets för ett ord i tokfile till korpus
+		// Hämta alla förekomster av ordet från tokfile och spara deras offsets
+		LinkedList<Long> results = new LinkedList<Long>();
 		raf = new RandomAccessFile(tokfile, "r");
-		RandomAccessFile rafKorpus = new RandomAccessFile(korpus, "r");
 		raf.seek(tokfilePos);
-		
 		for(long i=tokfilePos; i<tokfileNextPos; i=raf.getFilePointer()) {
 			String line[] = raf.readLine().split(" ");
-			Long posKorpus = Long.parseLong(line[1]);
-			byte[] b = new byte[30+args[0].length()+30];
-			rafKorpus.seek(posKorpus-30);
-			rafKorpus.read(b);
-			
-			System.out.println(new String(b));
+			results.add(Long.parseLong(line[1]));
 		}
+		raf.close();
+		
+		System.out.println();
+		System.out.println("Hittade "+results.size()+" förekomster av ordet: \""+args[0]+"\". Sökningen tog "+(float)searchTime.stop()/1000+"s.");
+		if(results.size() > resultsLimit) {
+			System.out.println("Visa alla? [j/n]");
+			Scanner in = new Scanner(System.in);
+			if(in.nextLine().equals("j")) resultsLimit = results.size();
+		} else {
+			resultsLimit = results.size();
+		}
+		System.out.println();
 		
 		
-
+		// Skriv ut från korpus!
+		raf = new RandomAccessFile(korpus, "r");
+		byte[] b = new byte[30+args[0].length()+30];
+		
+		for(int i=0; i<resultsLimit; i++) {
+			raf.seek(results.poll()-30);
+			raf.read(b);
+			
+			System.out.println(i+1+": "+new String(trimOutput(b), "ISO-8859-1"));
+		}
+		System.out.println();
+		raf.close();
 	}
 
 	private static boolean checkIndexes() {
-		korpus = new File("/info/adk13/labb1/korpus");
-		tokfile = new File("/var/tmp/tokfile");
-		bucket = new File("/var/tmp/bucket");
-		wordIndex = new File("/var/tmp/wordIndex");
-
 		if(tokfile.exists() && bucket.exists() && wordIndex.exists())
 			return true;
 		return false;
@@ -102,8 +128,7 @@ public class Konkordans {
 	}
 
 
-	public static boolean createIndexes() throws IOException, InterruptedException{
-		//		createBucket();
+	private static boolean createIndexes() throws IOException, InterruptedException{
 //		createTokfile();
 		createWordIndex();
 		createBucket();
@@ -120,89 +145,91 @@ public class Konkordans {
 		return true;
 	}
 	
-	public static boolean createBucket() throws IOException{
+	private static boolean createBucket() throws IOException{
 		System.out.println("Creating bucket index...");
-		Scanner in = new Scanner(wordIndex, "UTF-8");
+		Scanner in = new Scanner(wordIndex, "ISO-8859-1");
 
 		RandomAccessFile out = new RandomAccessFile(bucket, "rw");
 		out.setLength(195120); //29*29*29 * 8 + 1*8. För att a=1, därför används inte index 0. 
 
 		String lastWord = "";
 		String line = "";
-		Long pos = new Long(0);
-		while(in.hasNextLine()){
-			line = in.nextLine();
-			pos += line.length()+1;
-			String[] lineSplit = line.split(" ");
-			String newWord = "";
-
-			//			if(lineSplit[0].length() < 3)
-			//				newWord = line.substring(0, lineSplit[0].length());
-			if(lineSplit[0].length() > 3)
-				newWord = line.substring(0, 3);
-
-			if(!newWord.equals(lastWord)){				
-				out.seek(getBucketPosition(newWord));
-				out.writeLong(pos);
-				lastWord = newWord;
-			}
-		}
-		out.close();
-		return true;
-	}
-
-	public static boolean createWordIndex() throws FileNotFoundException, UnsupportedEncodingException{
-		System.out.println("Creating wordIndex...");
-		Scanner in = new Scanner(tokfile, "ISO-8859-1");
-		PrintWriter writer = new PrintWriter(wordIndex, "UTF-8");
-		String lastWord = "";
-		String line = "";
 		long pos = 0;
 		while(in.hasNextLine()){
 			line = in.nextLine();
+			String[] lineSplit = line.split(" ");
+			String newWord = lineSplit[0];
+
+			if(lineSplit[0].length() > 3)
+				newWord = newWord.substring(0, 3);
+
+			if(!newWord.equals(lastWord)){				
+				out.seek(getBucketOffset(newWord));
+				out.writeLong(pos);
+				System.out.println("word: "+newWord+" seek: "+getBucketOffset(newWord)+" pos: "+pos);
+				lastWord = newWord;
+			}
 			pos += line.length()+1;
+		}
+		out.close();
+		in.close();
+		return true;
+	}
+
+	private static boolean createWordIndex() throws FileNotFoundException, UnsupportedEncodingException{
+		System.out.println("Creating wordIndex...");
+		Scanner in = new Scanner(tokfile, "ISO-8859-1");
+		PrintWriter writer = new PrintWriter(wordIndex, "ISO-8859-1");
+		String lastWord = "";
+		String line = "";
+		long pos = 0;
+		while(in.hasNextLine()) {
+			line = in.nextLine();
 			String[] shortWord = line.split(" ");
 			String newWord = shortWord[0];
 			if(!newWord.equals(lastWord)){ //om senast skrivna ordet inte är lika med det aktuella ordet, skriv det till filen. 
 				String a = newWord + " " + pos;
+				System.out.println(a);
 				writer.println(a);
 				lastWord = newWord;
 			}
+			pos += line.length()+1;
 		}
 		writer.close();
+		in.close();
 
 		return true;
 	}
+	
 
-	private static long getBucketPosition(String word){
-		long off = 0;
+	private static long getBucketOffset(String word){
+		long pos = 0;
 		if(word.length() > 3) word = word.substring(0, 3);
 		char[] letters = word.toLowerCase().toCharArray();
-		
 
-		/*
-		 * å = 134
-		 * ä = 132
-		 * ö = 148
-		 */
-
-		//97-122 = a-z
-		for(int i = 0; i < letters.length; i++){
-			
+		for(int i=0; i < letters.length; i++){
 			if((int)letters[i] >= 97 && (int)letters[i] <= 122) 
-				off += ((int)letters[0]-96)*Math.pow(29, i);
-
-			else if((int)letters[i] == 134)
-				off += ((int)letters[i]-(108))*Math.pow(29, i); //134-27+1
-			else if((int)letters[i] == 132)
-				off += ((int)letters[i]-(105))*Math.pow(29, i); //132-28+1
-			else if((int)letters[i] == 148)
-				off += ((int)letters[i]-(120))*Math.pow(29, i); //148-29+1
-
+				pos += ((int)letters[i]-96)*Math.pow(29, 2-i);
+			else if((int)letters[i] == 229)
+				pos += 27*Math.pow(29, 2-i); // å Ascii 134 UTF-8 229
+			else if((int)letters[i] == 228)
+				pos += 28*Math.pow(29, 2-i); // ä Ascii 132 UTF-8 228
+			else if((int)letters[i] == 246)
+				pos += 29*Math.pow(29, 2-i); // ö Ascii 148 UTF-8 246
 		}
-
-		return off;
-
+		return pos*8;
+	}
+	/**
+	 * Removes newlines, carriage returns and tabs and replaces them with a space.
+	 * @param b
+	 * @return b 
+	 */
+	private static byte[] trimOutput(byte[] b) {
+		for(int i=0; i<b.length; i++) {
+			if(b[i]==10 || b[i]==13 || b[i] == 9)
+				b[i]=32;
+		}
+		return b;
 	}
 
 }
