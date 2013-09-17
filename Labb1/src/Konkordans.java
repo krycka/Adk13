d3831ce8a2be572d26d05d1ca269ff7867f324ea
krycka <krycka@gmail.com>
2013-09-17 11:47:52 +0200

    small cleanup

--- Labb1/src/Konkordans.java
+++ Labb1/src/Konkordans.java
@@ -1,7 +1,5 @@
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
